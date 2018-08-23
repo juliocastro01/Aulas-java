497210d11d03ab1833538c4e26388a2c1810c4c1
juliocastro01 <42468386+juliocastro01@users.noreply.github.com>
2018-08-23 20:10:56 -0300

    Add files via upload

---
+++ OrdemCouD.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class OrdemCouD {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int aux = 0;
+        System.out.print("Quantos valores: ");
+        int n = scr.nextInt();
+        System.out.println("Ordem Crescente ou Decrescente: [c/d]");
+        String CouD = scr.next();
+        int v[] = new int[n];
+        for (int i = 0; i < v.length; i++) {
+            v[i] = scr.nextInt();
+        }
+        if(CouD.equals("c") || CouD.equals("C")){
+            for (int i = 0; i < v.length; i++) {
+                for (int j = 0; j < v.length; j++) {
+                    if(v[i] < v[j]){
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            }
+        }else if(CouD.equals("d") || CouD.equals("D")){
+            for (int i = 0; i < v.length; i++) {
+                for (int j = 0; j < v.length; j++) {
+                    if(v[i] > v[j]){
+                        aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < v.length; i++) {
+            System.out.print(v[i] + " ");
+        }
+    }
+}
---
+++ Receber.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Receber {
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int a  = scr.nextInt();
+        int b  = scr.nextInt();
+        
+        if(a > b){
+            System.out.println(a + " é o maior");
+        }else if(a < b){
+            System.out.println(b + " é o maior");
+        }else{
+            System.out.println("São iguais");
+        }
+    }
+}
